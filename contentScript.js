62eb1d3f140ac4ca24e235e4666ef12d20fa4f9e
Zheltiy DA <zda@mogt.ru>
2019-08-15 14:22:06 +0300

    Add localstorage

--- contentScript.js
+++ contentScript.js
@@ -1,17 +1,22 @@
+let bar = document.querySelector(".bar");
+let bar__content = bar.querySelector(".bar__content");
+let player = bar__content.querySelector(".deco-player-controls");
+
 chrome.runtime.onMessage.addListener(
     function(request, sender, sendResponse) {
 
-      console.log(sender.tab ?
-                  "from a content script:" + sender.tab.url :
-                  "from the extension");
-      if (request.greeting == "hello")
-        sendResponse({farewell: "goodbye"});
+        console.log(sender.tab ?
+                    "from a content script:" + sender.tab.url :
+                    "from the extension");
 
-        let bar = document.querySelector(".bar");
-        let bar__content = bar.querySelector(".bar__content");
-        let player = bar__content.querySelector(".deco-player-controls");
-
-        bar__content.style.background = "red";
-        player.style.background = "none";
+        if (request.greeting) {
+            sendResponse({farewell: "goodbye"});
+            bar__content.style.background = "red";
+            player.style.background = "none";
+        } else {
+            sendResponse({farewell: "goodbye"});
+            bar__content.style.background = '';
+            player.style.background = '';
+        }
     }
 );
--- popup.js
+++ popup.js
@@ -1,9 +1,33 @@
 document.addEventListener("DOMContentLoaded", function(event) {
-    document.querySelector('#hidcheck').addEventListener('click', function() {
+    document.querySelector('#hidcheck').checked = (localStorage.getItem("stateTheme") === 'true');
+
+    if ((localStorage.getItem("stateTheme") === 'true')) {
         chrome.tabs.query({active: true, currentWindow: true}, function(tabs) {
-            chrome.tabs.sendMessage(tabs[0].id, {greeting: "hello"}, function(response) {
-              console.log(response.farewell);
+            chrome.tabs.sendMessage(tabs[0].id, {greeting: true}, function(response) {
+            console.log(response.farewell);
             });
         });
+    }
+
+    document.querySelector('#hidcheck').addEventListener('click', function() {
+        if (localStorage.getItem("stateTheme") === 'true' || localStorage.getItem("stateTheme") === 'false') {
+            localStorage.setItem("stateTheme", !(localStorage.getItem("stateTheme") === 'true'));
+        } else {
+            localStorage.setItem("stateTheme", true);
+        }
+
+        if ((localStorage.getItem("stateTheme") === 'true')) {
+            chrome.tabs.query({active: true, currentWindow: true}, function(tabs) {
+                chrome.tabs.sendMessage(tabs[0].id, {greeting: true}, function(response) {
+                console.log(response.farewell);
+                });
+            });
+        } else {
+            chrome.tabs.query({active: true, currentWindow: true}, function(tabs) {
+                chrome.tabs.sendMessage(tabs[0].id, {greeting: false}, function(response) {
+                console.log(response.farewell);
+                });
+            });
+        }
     });
 });
