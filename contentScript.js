fd1b60e5a3c64ca1f9cc06f1d11a9918c4c7944a
Zheltiy DA <zda@mogt.ru>
2019-08-15 13:29:47 +0300

    Add sending data to the scripts

--- contentScript.js
+++ contentScript.js
@@ -1,53 +1,17 @@
-window.onload = function() {
-    let bar = document.querySelector(".bar");
-    let bar__content = bar.querySelector(".bar__content");
-    let player = bar__content.querySelector(".deco-player-controls");
-}
-
 chrome.runtime.onMessage.addListener(
     function(request, sender, sendResponse) {
-        alert("success");
+
+      console.log(sender.tab ?
+                  "from a content script:" + sender.tab.url :
+                  "from the extension");
+      if (request.greeting == "hello")
+        sendResponse({farewell: "goodbye"});
+
+        let bar = document.querySelector(".bar");
+        let bar__content = bar.querySelector(".bar__content");
+        let player = bar__content.querySelector(".deco-player-controls");
+
         bar__content.style.background = "red";
         player.style.background = "none";
     }
 );
-
-//let port = chrome.runtime.connect();
-
-// window.onload = function() {
-//     let bar = document.querySelector(".bar");
-//     let bar__content = bar.querySelector(".bar__content");
-//     let player = bar__content.querySelector(".deco-player-controls");
-
-//     bar__content.style.background = "red";
-//     player.style.background = "none";
-
-//     window.addEventListener("message", function(event) {
-//         alert("Recievved");
-//       // We only accept messages from ourselves
-//       if (event.source != window)
-//         return;
-
-//       if (event.data.type && (event.data.type == "FROM_PAGE")) {
-//         console.log("Content script received: " + event.data.text);
-//         alert(event.data.text);
-//         port.postMessage(event.data.text);
-//       }
-//     }, false);
-//     console.log('onload');
-//     let tablink = '';
-
-//     chrome.tabs.query({'active': true, 'lastFocusedWindow': true}, function (tabs) {
-//         tablink = tabs[0].url;
-//         console.log(tablink);
-//     });
-
-//     if (tablink.includes("music.yandex")) {
-//         let bar = document.querySelector(".bar");
-//         let bar__content = bar.querySelector(".bar__content");
-
-//         document.querySelector("#hidcheck").addEventListener('click', function() {
-//             alert("Сработал");
-//         });
-//     }
-//};
--- popup.js
+++ popup.js
@@ -1,56 +1,10 @@
-// Проверяет state у инпута
-// function changeState2() {
-//     console.log('Зашёл2');
-//     if(this.checked) {
-//         bar__content.style.background = "red";
-//     } else {
-//         bar__content.style.background = "";
-//     }
-// };
-
 document.addEventListener("DOMContentLoaded", function(event) {
     document.querySelector('#hidcheck').addEventListener('click', function() {
-        chrome.runtime.sendMessage({greeting: "hello"}, function(response) {
-            alert('Send' + response);
+        chrome.tabs.query({active: true, currentWindow: true}, function(tabs) {
+            chrome.tabs.sendMessage(tabs[0].id, {greeting: "hello"}, function(response) {
+              console.log(response.farewell);
+              alert('Send' + response);
+            });
         });
-        // if(this.checked) {
-        //     var div = document.createElement('div');
-        //     var label = document.createElement('span');
-        //     label.textContent = "Hello, world";
-        //     div.appendChild(label);
-        //     document.body.appendChild(div);
-        // } else {
-        //     bar__content.style.background = "";
-        // }
     });
 });
-
-// window.onload = function() {
-//     let bar = document.querySelector(".bar");
-//     let bar__content = bar.querySelector(".bar__content");
-//     let player = bar__content.querySelector(".deco-player-controls");
-
-//     function draw() {
-//         bar__content.style.background = "red";
-//         player.style.background = "none";
-//     }
-
-//     // bar__content.style.background = "red";
-//     // player.style.background = "none";
-// //     console.log('onload');
-// //     let tablink = '';
-
-// //     chrome.tabs.query({'active': true, 'lastFocusedWindow': true}, function (tabs) {
-// //         tablink = tabs[0].url;
-// //         console.log(tablink);
-// //     });
-
-// //     if (tablink.includes("music.yandex")) {
-// //         let bar = document.querySelector(".bar");
-// //         let bar__content = bar.querySelector(".bar__content");
-
-// //         document.querySelector("#hidcheck").addEventListener('click', function() {
-// //             alert("Сработал");
-// //         });
-// //     }
-// };
